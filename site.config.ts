1eb60c737bfbda53a47ead87498c0b77ff2edfa3
cl <64207173+xchlu@users.noreply.github.com>
2022-06-05 16:07:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '7a9d88c7a8604fe8b1bac83f9a9409e2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'CL Blog',
+  domain: 'https://www.notion.so/Personal-Blog-7a9d88c7a8604fe8b1bac83f9a9409e2',
+  author: 'Chang Lu',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+//   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+//   twitter: '',
+  github: 'xchlu',
+//   linkedin: '',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
